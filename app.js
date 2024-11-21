387a23da33f9479176e39d5e2909bc6d446de7f4
Talha <tk033313@gmail.com>
2024-11-21 19:01:41 +0500

    comit  changes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize=require('express-mongo-sanitize')
+// const helmet=require('helmet')
 
 const userRoutes = require('./routes/users');
 const reviewRoutes = require('./routes/reviews');
@@ -35,6 +36,10 @@ app.engine('ejs',ejsMate)
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
+// app.use(helmet({
+//     ccontentSecurityPolicy : false,
+    
+// }))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname,'public')))
